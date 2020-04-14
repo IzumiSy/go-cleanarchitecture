8924fb562c6f6b6d9e8e480d671728b81439781b
IzumiSy <beetle-noise@gmx.com>
2020-04-14 23:18:44 +0900

    Removed comments

--- domains/logger.go
+++ domains/logger.go
@@ -1,13 +1,10 @@
 package domains
 
+// ロガー実装の抽象
+
 type Logger interface {
 	Debug(msg string)
 	Error(msg string)
 	Info(msg string)
 	Warn(msg string)
-
-	// Debugf(template string, args ...interface{})
-	// Errorf(template string, args ...interface{})
-	// Infof(template string, args ...interface{})
-	// Warnf(template string, args ...interface{})
 }
