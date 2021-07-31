cb11bbae6788f37efb352fb2418205c59901e382
IzumiSy <beetle-noise@gmx.com>
2021-07-31 15:29:38 +0900

    wip

--- dredd_hook.js
+++ dredd_hook.js
@@ -1,3 +1,15 @@
 var hooks = require('hooks')
 
 hooks.beforeAll(function(transactions) { })
+
+session = {}
+
+after('/login > POST', function(transaction) {
+  session.token = JSON.parse(transaction.real.body).session_id
+})
+
+beforeEach(function(transaction) {
+  if (session.token) {
+    transaction.request.headers['Authorization'] = `bearer ${session.token}`
+  }
+})
