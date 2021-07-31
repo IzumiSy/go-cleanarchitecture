c7a6d7d9a5c80807b41baac0c4117d2f4175296e
IzumiSy <beetle-noise@gmx.com>
2021-07-31 15:51:06 +0900

    wip

--- dredd_hook.js
+++ dredd_hook.js
@@ -2,14 +2,43 @@ var hooks = require('hooks')
 
 hooks.beforeAll(function(transactions) { })
 
-session = {}
+session = {
+  token: null,
+  email: null,
+  todoName: null
+}
 
-after('/login > POST', function(transaction) {
+hooks.beforeAll(function(transaction) {
+  var prefix = Math.round(Math.random() * 10000)
+  session.email = `user${prefix + ''}@example.com`
+  session.todoName = `todo ${prefix + ''}`
+  console.log("Email:", session.email)
+})
+
+hooks.before('/signup > POST', function(transaction) {
+  var requestBody = JSON.parse(transaction.request.body);
+  requestBody['email'] = session.email
+  transaction.request.body = JSON.stringify(requestBody)
+})
+
+hooks.before('/login > POST', function(transaction) {
+  var requestBody = JSON.parse(transaction.request.body);
+  requestBody['email'] = session.email
+  transaction.request.body = JSON.stringify(requestBody)
+})
+
+hooks.after('/login > POST', function(transaction) {
   session.token = JSON.parse(transaction.real.body).session_id
 })
 
-beforeEach(function(transaction) {
+hooks.beforeEach(function(transaction) {
   if (session.token) {
-    transaction.request.headers['Authorization'] = `bearer ${session.token}`
+    transaction.request.headers['Authorization'] = `Bearer ${session.token}`
   }
+})
+
+hooks.before('/todo > POST', function(transaction) {
+  var requestBody = JSON.parse(transaction.request.body);
+  requestBody['name'] = session.todoName
+  transaction.request.body = JSON.stringify(requestBody)
 })
